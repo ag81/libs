95adbfa97eb91628e62990158bdd5b6766d4680c
asc-b <txomin.azpiroz@alumni.mondragon.edu>
2013-06-07 09:12:35 +0200

    feature sendCAN segunda version

--- CANlib/MsgQueuePoolConfig.h
+++ CANlib/MsgQueuePoolConfig.h
@@ -1,14 +1,11 @@
 #ifndef MSGQUEUEPOOLCONFIG_H
 #define MSGQUEUEPOOLCONFIG_H
 
-#define TECHO 0x101
 
-//const unsigned int techo = 0x101;
-
-const unsigned int receivedMsgIDs[IN_QUEUES]={0x102,0x201,0x301};
+const unsigned int receivedMsgIDs[IN_QUEUES]={0x102,0x202,0x301};
 const unsigned int receivedMsgDataLen[IN_QUEUES]={4,4,4};
 //const unsigned int transmitedMsgIDs[OUT_QUEUES]={TECHO};
-const unsigned int transmitedMsgIDs[OUT_QUEUES]={TECHO,0x202,0x302};
+unsigned int transmitedMsgIDs[OUT_QUEUES];
 const unsigned int transmitedMsgDataLen[OUT_QUEUES]={4,4,4};
 #define RECEIVE_MESSAGE_MASK    0xFFF
 #define CAN_BITRATE             500000
